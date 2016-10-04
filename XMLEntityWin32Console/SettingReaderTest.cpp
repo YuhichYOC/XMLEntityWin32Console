17718f0468a8c33631d2b5129bed5a04d9aa0302
b50061tem <b50061tem@OEC603>
2016-10-04 15:27:49 +0900

    作業状態の保存

---
+++ XMLEntityWin32Console/SettingReaderTest.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+
+#include "SettingReaderTest.h"
+
+bool SettingReaderTest::GetTestSuccess()
+{
+    return testSuccess;
+}
+
+void SettingReaderTest::SettingReaderTest01()
+{
+
+}
+
+SettingReaderTest::SettingReaderTest()
+{
+}
+
+SettingReaderTest::~SettingReaderTest()
+{
+}
---
+++ XMLEntityWin32Console/SettingReaderTest.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "SettingReader.h"
+
+class SettingReaderTest
+{
+private:
+
+    bool testSuccess;
+
+public:
+
+    bool GetTestSuccess();
+
+    void SettingReaderTest01();
+
+    SettingReaderTest();
+
+    ~SettingReaderTest();
+};
+
